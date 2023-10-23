274f6636ebd4f4e7474323eb47f07b113547e717
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-10-23 11:54:37 +0100

    componentDidMount componentDidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,22 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
   formSubmitData = data => {
     const currentContact = this.state.contacts.find(
       contact =>
